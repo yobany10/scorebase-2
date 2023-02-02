64088a8e31fb684963feab9edb76d931a3c9ca94
Yobany Perez <yobanyperez@Yobanys-MacBook-Pro.local>
2023-02-02 14:49:32 -0500

    Added logout feature.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,24 @@ import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import {auth} from '../Firebase/config'
 import {useAuthState} from 'react-firebase-hooks/auth'
+import {toast} from 'react-toastify'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown';
 import './Navbar.css'
 
 const MainNavbar = () => {
     const navigate = useNavigate()
     const [user, loading] = useAuthState(auth)
+
+    const toastConfig = {
+        position: 'bottom-right'
+    }
+
+    const handleLogoutClick = () => {
+        auth.signOut()
+        toast.info('You have been signed out', toastConfig)
+    }
+
     return (
         <>
         <nav className="navbar-div">
@@ -18,7 +31,11 @@ const MainNavbar = () => {
                     {!user && <p className='navbar-link navbar-login' onClick={() => navigate('/login')}>Login</p>}
                     {user && <p className='navbar-link navbar-dashboard' onClick={() => navigate('/dashboard')}>Dashboard</p>}
                     {user && <p className='navbar-link navbar-scorekeep' onClick={() => navigate('/scorekeeping')}>Scorekeep</p>}
-                    {user && <img src={user.photoURL} className='navbar-user-avatar' referrerPolicy="no-referrer" />}
+                    {user &&
+                        <DropdownButton className='navbar-user-dropdown-button' bsPrefix='navbar-user-dropdown-button' title={<img src={user.photoURL} className='navbar-user-avatar' referrerPolicy="no-referrer" />}>
+                            <Dropdown.Item onClick={handleLogoutClick}>Logout</Dropdown.Item>
+                        </DropdownButton>
+                    }
                 </div>
             </div>
         </nav>
